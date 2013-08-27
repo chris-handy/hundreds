057d58fbbd68e203ce816eef0ce2ce51282d0f55
chris-handy <chris.handy9@gmail.com>
2013-08-26 21:40:01 -0400

    re-did ball drawing code to use KineticJS instead of default javascript due to event architecture.

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,81 @@
-/***********************************
-	initial variables 
-**************************************/
-var canvas = document.getElementById('stage');
-var circle = canvas.getContext('2d');
-var circleX = canvas.width/2;
-var circleY = canvas.height/2;
-var radius = 50;
-var dx = 3;
-var dy = 3;
+/*******************************
+	setting up the stage/layers
+********************************/
+var stage = new Kinetic.Stage({
+	container : 'stage',
+	width : 900,
+	height : 600
+});
+var shapesLayer = new Kinetic.Layer();
 
-/********************************************
-	Load when the document does.
-*******************************************/
-	window.onload = function(e){
-		init();
-		console.log('hello');
+/**********************************
+	creating the circle object
+*************************************/
+var circle = new Kinetic.Circle({
+	x : stage.getWidth() / 2,
+	y : stage.getHeight() / 2,
+	radius : 50,
+	fill : 'grey',
+	stroke : 'black',
+	strokeWidth : 1
+});
+
+/************************************************
+	create the binding box group for the circle
+**********************************************/
+/*var bindingBox = new Kinetic.Group({
+	x : stage.getWidth(),
+	y : stage.getHeight(),
+	dragBoundFunc: function(pos){
+		var x = stage.getWidth();
+		var y = stage.getHeight();
+		if(pos.x < 0){newX = 0}
+		if(pos.x > stage.getWidth()){newX = stage.getWidth()}
+		if(pos.y < 0){newY = 0}
+		if(pos.y > stage.getHeight()){newY = stage.getHeight()}
+
 	}
-/******************************************
-	Having the circle move around randomly
-*********************************************/
+});
+*/
+/*************************************
+	add the circle to the stage
+**************************************/
+//bindingBox.add(circle);
+shapesLayer.add(circle);
+stage.add(shapesLayer);
 
-function init(){
-	setInterval(drawCircle, 10);
-}
+/************************************************
+	creating the animation to move the circle
+*************************************************/
+var velocity = 50;
 
+var anim = new Kinetic.Animation(function(frame) {
+	var dist = velocity * (frame.timeDiff / 100);
+	//console.log('adfasdf');
+	console.log(circle.getY());
+	circle.move(dist, 0);
+	if(circle.getX() - 50 < 0){
+		console.log('passed 0 stage width');
+		//circle.setX(0);
+		circle.move(0, 0);
+	}
+	if(circle.getX() + 50 > stage.getWidth() ){
+		console.log('passed max stage width');
+		circle.setX(stage.getWidth()-50);
+	}
+	if(circle.getY() - 50 < 0){
+		console.log('passed 0 stage height');
+		circle.setY(0);
+	}
+	if(circle.getY() + 50 > stage.getHeight()){
+		console.log('passed max stage height');
+		circle.setY(stage.getHeight()-50);
+		//throw 'staahp';
 
-function drawCircle(){
-//draw the circle and have it move
-	circle.clearRect(0, 0, 900, 600);
-	circle.beginPath();
-	circle.arc(circleX, circleY, radius, 0, 2 * Math.PI, false);
-	circle.closePath();
-	circle.fillStyle = 'grey';
-	circle.fill();
-	circle.lineWidth = 1;
-	circle.strokeStyle = '#000';
-	circle.stroke();
-	circleX += dx;
-	circleY += dy;
-//boundary logic
-	if (circleX - 50 < 0 || circleX + 50 > 900) dx =- dx;
-	if (circleY - 50 < 0 || circleY + 50 > 600) dy =- dy;
-}
+	}
+	
+}, shapesLayer);
 
+anim.start();
+
+//http://www.html5canvastutorials.com/labs/html5-canvas-interactive-ball-physics/

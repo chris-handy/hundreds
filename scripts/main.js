6a75edfd14bb7f683443d9a021950a3fad77c3fa
chris-handy <chris.handy9@gmail.com>
2013-09-03 17:38:44 -0400

    re-doing the ball code, its good

--- scripts/main.js
+++ scripts/main.js
@@ -8,141 +8,68 @@ window.onload = function(){
 		height : 600
 	});
 	var shapesLayer = new Kinetic.Layer();
-
+	stage.add(shapesLayer);
 	/**********************************
 		creating the circle object
 	*************************************/
 	var circle = new Kinetic.Circle({
 		x : stage.getWidth() / 2,
 		y : stage.getHeight() / 2,
-		radius : 50,
+		radius : 10,
 		fill : 'grey',
 		stroke : 'black',
 		strokeWidth : 1
 	});
 
-	//the circles custom properties 
-	ball.vx = 0;
-    ball.vy = 0;
+	//circle properties 
+	circle.X = stage.getWidth() / 2;
+	circle.Y = stage.getHeight() / 2;
+	circle.VX = 5;
+	circle.VY = 5;
+	circle.RAF;
+	//make the circle move
+	circle.move=function(){
+	    // calc new balloon position
+	    this.X+=this.VX;
+	    this.Y+=this.VY;
+	    console.log(stage);
+	    
+	    // reverse if colliding
+	    var radius=this.getRadius();
+	    if(this.X < radius){ this.VX *= -1; this.X = radius; }
+	    if(this.X > stage.getWidth()-radius){ this.VX *= -1; this.X = stage.getWidth()-radius; }
+	    if(this.Y < radius){ this.VY *= -1; this.Y = radius; }
+	    if(this.Y > stage.getHeight()-radius){ this.VY *= -1; this.Y = stage.getHeight()-radius; }
 
-    //add event listeners for mouseover/out/onclick
-    
-
-	/*************************************
-		add the circle to the stage
-	**************************************/
-	//bindingBox.add(circle);
-	shapesLayer.add(circle);
-	stage.add(shapesLayer);
-
-	var date = new Date();
-    var time = date.getTime();
-    animate(time, ball);
-}
-
-window.requestAnimFrame = (function(callback){
-	return window.requestAnimationFrame ||
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame ||
-	window.oRequestAnimationFrame ||
-	window.msRequestAnimationFrame ||
-	function(callback){
-		window.setTimeout(callback, 1000 / 60);
-	};
-})();	
-
-function animate(lastTime, circle){
-	var stage = circle.getStage();
-	var shapesLayer = circle.getLayer();
-	var date = new Date();
-	var time = date.getTime();
-	var timeDiff = time - lastTime;
-
-	// update
-	updateCircle(timeDiff, circle);
-
-	// draw
-	shapesLayer.draw();
-
-	// request new frame
-	requestAnimFrame(function(){
-		animate(time, circle);
+	    // set the new balloon position
+	    this.setPosition(this.X,this.Y);
+	    // draw it
+		shapesLayer.draw();
+	    // request a new animation loop
+	    var b=this;
+	    this.RAF=requestAnimationFrame(function(){ b.move();});
+	}
+	//make the circle expand when clicked on
+	circle.tw;
+	circle.on("mousedown",function(){
+		cancelAnimationFrame(this.RAF);
+	    this.tw=new Kinetic.Tween({
+	        node:this,
+	        duration:3,
+	        radius:100
+	    });
+	    this.tw.play();
 	});
+	//make the circle stop expanding and return moving;
+	circle.on("mouseup",function(){
+	    this.tw.pause();
+	    this.move();
+	});
+
+	//add the circle to the shapelayer and make it move
+	shapesLayer.add(circle);
+	circle.move()
+	shapesLayer.draw();
 }
- function updateCircle(timeDiff, circle){
-	var stage = circle.getStage();
-	var circleX = circle.x;
-	var circleY = circle.y;
+	
 
-	// physics variables
-	var gravity = 20; // px / second^2
-	var speedIncrementFromGravityEachFrame = gravity * timeDiff / 1000;
-	var collisionDamper = 0.2; // 20% energy loss
-	var floorFriction = 5; // px / second^2
-	var floorFrictionSpeedReduction = floorFriction * timeDiff / 1000;
-
-	// if dragging and dropping
-	if (circle.drag.moving) {
-		var mousePos = stage.getMousePosition();
-
-		if (mousePos !== null) {
-			var mouseX = mousePos.x;
-			var mouseY = mousePos.y;
-
-			var c = 0.06 * timeDiff;
-			circle.vx = c * (mouseX - circle.lastMouseX);
-			circle.vy = c * (mouseY - circle.lastMouseY);
-			circle.lastMouseX = mouseX;
-			circle.lastMouseY = mouseY;
-		}
-	}
-	else {
-		// gravity
-		circle.vy += speedIncrementFromGravityEachFrame;
-		circleX += circle.vx;
-		circleY += circle.vy;
-
-		// ceiling condition
-		if (circleY < circle.radius) {
-			circleY = circle.radius;
-			circle.vy *= -1;
-			circle.vy *= (1 - collisionDamper);
-		}
-
-		// floor condition
-		if (circleY > (stage.height - circle.radius)) {
-			circleY = stage.height - circle.radius;
-			circle.vy *= -1;
-			circle.vy *= (1 - collisionDamper);
-		}
-
-		// floor friction
-		if (circleY == stage.height - circle.radius) {
-			if (circle.vx > 0.1) {
-				circle.vx -= floorFrictionSpeedReduction;
-			}
-			else if (circle.vx < -0.1) {
-				circle.vx += floorFrictionSpeedReduction;
-			}
-			else {
-				circle.vx = 0;
-			}
-		}
-
-		// right wall condition
-		if (circleX > (stage.width - circle.radius)) {
-			circleX = stage.width - circle.radius;
-			circle.vx *= -1;
-			circle.vx *= (1 - collisionDamper);
-		}
-
-		// left wall condition
-		if (circleX < (circle.radius)) {
-			circleX = circle.radius;
-			circle.vx *= -1;
-			circle.vx *= (1 - collisionDamper);
-		}
-	}
-
-	circle.setPosition(circleX, circleY);
-}
